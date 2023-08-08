fc860515c63edc93ee0717bfdf9ca21167e768da
dheeraj1429 <dheerajsingh1429@gmail.com>
2023-08-08 15:40:49 +0530

    nest-server config change

--- lottery-server/src/main.ts
+++ lottery-server/src/main.ts
@@ -3,7 +3,10 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-   const app = await NestFactory.create(AppModule, { cors: true });
+   const app = await NestFactory.create(AppModule, {
+      cors: true,
+      logger: console,
+   });
    app.useGlobalPipes(new ValidationPipe());
    await app.listen(process.env.PORT);
 }
