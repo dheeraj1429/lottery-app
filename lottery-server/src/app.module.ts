8271f648842a4c75a46b1badd42b79ccfd344452
dheeraj1429 <dheerajsingh1429@gmail.com>
2023-08-08 15:56:29 +0530

    env config changes

--- lottery-server/src/app.module.ts
+++ lottery-server/src/app.module.ts
@@ -14,7 +14,7 @@ import { LuckyDrawModule } from './lucky-draw/lucky-draw.module';
 @Module({
    imports: [
       ConfigModule.forRoot({
-         envFilePath: ['.env.development'],
+         envFilePath: ['.env'],
          isGlobal: true,
       }),
 
